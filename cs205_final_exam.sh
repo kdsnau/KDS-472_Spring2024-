0244343662fc37b6bd6712e816042183c702c494
kdsnau <166072347+kdsnau@users.noreply.github.com>
2024-05-07 22:11:25 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,12 +1,21 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ===== SUMMARY OF DATA FILE =====
-#    File name: [VALUE]
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ===== END SUMMARY =====
+#!/bin/bash
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+# Set the filename as the first positional parameter passed to the script
+filename=$1
+
+# Calculate the total number of Pokemon
+total_pokemon=$(awk 'END {print NR}' "$filename")
+
+# Calculate the average HP and average Attack using awk
+avg_hp=$(awk '{total += $2} END {print total/NR}' "$filename")
+avg_attack=$(awk '{total += $3} END {print total/NR}' "$filename")
+
+# Print the summary report
+echo "===== SUMMARY OF DATA FILE ====="
+echo "   File name: $filename"
+echo "   Total Pokemon: $total_pokemon"
+echo "   Avg. HP: $avg_hp"
+echo "   Avg. Attack: $avg_attack"
+echo "===== END SUMMARY ====="
+
+
